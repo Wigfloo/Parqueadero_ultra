da0790ae4d2c506056c9c6d7279b11508b8fc139
JCodeMasterJ <juanescajasan@gmail.com>
2023-10-11 12:23:02 -0500

    COMMIT ACTUALIZACIÓN CSS - FUNCION REGISTRAR USUARIO
    SE APLICAN CAMBIOS DE ESTILIZACIÓN CSS - SE ACTUALIZA FUNCIO DE REGIUSTRAR USUARIO, SE CONECTA CON FETCH A REGUSTRAR USUARIO, CONEXION FUNCIONA OK PERO HAY PROBLEMA DE DUPLICACION DE LALVE PRIMARIA, NO FUNCIONA EL INICIO DE SESION

--- SmartParkingEnterprises/JavaScript/script_registro.js
+++ SmartParkingEnterprises/JavaScript/script_registro.js
@@ -10,6 +10,7 @@ loginLink.addEventListener("click", () => {
   wrapper.classList.remove("active");
 });
 
+/* ESTA FUNCIÓN SE EJECUTA CUANDO SE INICIA SESIÓN */
 function login() {
   var nombre1 = "se envió el nombre1";
   console.log(nombre1);
@@ -52,7 +53,7 @@ function login() {
 
   $.ajax({
     data: datos,
-    url: "http://localhost/SmartParkingEnterprise/registrarusuario.php",//antes era checklogin
+    url: "http://localhost/SmartParkingEnterprise/checklogin.php",//antes era checklogin
     //cache: false,
     dataType: "json",
     type: "POST",
@@ -74,11 +75,54 @@ function login() {
   });
 }
 
-/**COMENTAR CADA FUNCIÓN  */
+/**ESTA FUNCIÓN SE EJECUTA CUANDO SE CREA UN RGISTRO*/
 function register() {
-  alert("aca voy")
-  console.log("Se envio el nombre2");
-  formulario2.reset();
-  return false;
+
+  /* esto lo agregó Ricardo */
+  /**SE DEBEN AGREGAR LOS DEMÁS DATOS SI APLICA */
+  var username = $("#input_username_registro").val();
+  console.log("🚀 ~ file: script_registro.js:21 ~ login ~ username:", username);
+  if (username === "") {Swal.fire("Debe ingresar nombre de usuario!", "", "error");return;}
+
+  var email = $("#input_email_registro").val();
+  console.log("🚀 ~ file: script_registro.js:28 ~ login ~ email:",email);
+  if (email === "") {Swal.fire("Debe ingresar email!","El campo de email es obligatorio!!","warning");return;}
+
+
+  var password = $("#input_password_registro").val();
+  console.log("🚀 ~ file: script_registro.js:28 ~ login ~ password:",password);
+  if (password === "") {Swal.fire("Debe ingresar password!", "El campo de password es obligatorio!!","warning"); return;}
+
+  datos = { username, email, password };
+  console.log("🚀 ~ file: script_registro.js:44 ~ login ~ datos:", datos);
+
+  /**
+   *
+   *ACÁ CONFIGURO UN API WEB QUE CONECTE CON EL BACKEND EN PHP
+   *
+   */
+
+  $.ajax({
+    data: datos,
+    url: "http://localhost/SmartParkingEnterprise/registrarusuario.php",//antes era checklogin
+    //cache: false,
+    dataType: "json",
+    type: "POST",
+    beforeSend: function () {
+      formulario1.reset();
+    },
+    success: function (respuesta) {
+      console.log("rta: ", respuesta);
+      Swal.fire("" + respuesta.message, "", "success");
+      /***acá se manejan los estados del servidor o las rtas del server */
+    },
+    error: function (jqXHR, textStatus) {
+      Swal.fire(
+        "!Parece que hay un error en la solicitud AJAX¡",
+        "" + textStatus,
+        "warning"
+      );
+    },
+  });
 }
 
