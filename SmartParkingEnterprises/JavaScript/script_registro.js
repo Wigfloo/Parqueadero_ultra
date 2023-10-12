cc7a3a25708b05f71fd76fb1328fe08d415ba958
ricar medina <rampmail29@gmail.com>
2023-10-11 22:25:12 -0500

    COMMIT MANEJO DE ESTADOS DEL SRVIDOR
    Se confirguran y adecuan los manejos de estados del servidor para el módulo de login y el módulo de registro de usuarios

--- SmartParkingEnterprises/JavaScript/script_registro.js
+++ SmartParkingEnterprises/JavaScript/script_registro.js
@@ -23,20 +23,17 @@ function login() {
   console.log("🚀 ~ file: script_registro.js:21 ~ login ~ email:", email);
 
   if (email === "") {
-    Swal.fire("Debe ingresar cuenta de correo!", "", "error");
+    Swal.fire("!Debe ingresar cuenta de correo!", "", "error");
     return;
   }
 
   var password = $("#input_password").val();
-  console.log(
-    "🚀 ~ file: script_registro.js:28 ~ login ~ password:",
-    password
-  );
+  console.log("🚀 ~ file: script_registro.js:28 ~ login ~ password:", password);
 
   if (password === "") {
     Swal.fire(
-      "Debe ingresar password!",
-      "El campo de contraseña es obligatorio!!",
+      "!Debe ingresar password!",
+      "El campo de contraseña es obligatorio.",
       "warning"
     );
     return;
@@ -53,45 +50,103 @@ function login() {
 
   $.ajax({
     data: datos,
-    url: "http://localhost/SmartParkingEnterprise/checklogin.php",//antes era checklogin
+
+    url: "http://localhost/SmartParkingEnterprise/checklogin.php", //antes era checklogin
+
     //cache: false,
     dataType: "json",
     type: "POST",
     beforeSend: function () {
       formulario1.reset();
     },
+    /***acá se manejan los estados del servidor o las rtas del server */
     success: function (respuesta) {
-      console.log("rta: ", respuesta);
-      Swal.fire("" + respuesta.message, "", "success");
-      /***acá se manejan los estados del servidor o las rtas del server */
+      console.log("rta: ", respuesta.error);
+      /* SI SE INGRESAN DATOS ALEATORIOS QUE NO EXISTAN EN LA BD */
+      if (
+        respuesta.error ===
+        "No se encontraron registros con los datos suministrados"
+      ) {
+        Swal.fire("" + respuesta.error, "", "info");
+        return;
+      }
+      /* SI LAS CREDENCIALES SON ERRONEAS */
+      if (respuesta.error === "Usuario o contraseña están incorrectos.") {
+        Swal.fire("" + respuesta.error, "", "error");
+        return;
+      }
+      /* SI HAY ERROR AL CONSULTAR EL ROL */
+      if (
+        respuesta.error ===
+        "Error al consultar roles. Contactar al administrador."
+      ) {
+        Swal.fire("" + respuesta.error, "", "error");
+        return;
+      }
+      /* SI EL INGRESO ES EXITOSO */
+      if (respuesta.id > 0) {
+        Swal.fire(
+          "Bienvenid@ " + respuesta.nombre,
+          "Ingresaste como " + respuesta.roll,
+          "success"
+        );
+        return;
+      }
     },
-    error: function (jqXHR, textStatus) {
-      Swal.fire(
-        "!Parece que hay un error en la solicitud AJAX¡",
-        "" + textStatus,
-        "warning"
-      );
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.log("Error status: " + textStatus);
+      console.log("Error thrown: " + errorThrown);
+      console.log("Response: " + jqXHR.status);
+      if (jqXHR.status === 500) {
+        // Si el código de estado es 500
+        Swal.fire(
+          "¡Error en el servidor!",
+          "Ha ocurrido un error interno en el servidor. Por favor, intenta nuevamente más tarde.",
+          "error"
+        );
+      } else {
+        Swal.fire(
+          "!Parece que hay un error en la solicitud AJAX¡",
+          "" + textStatus,
+          "warning"
+        );
+      }
     },
   });
 }
 
-/**ESTA FUNCIÓN SE EJECUTA CUANDO SE CREA UN RGISTRO*/
+/**ESTA FUNCIÓN SE EJECUTA CUANDO SE CREA UN REGISTRO*/
 function register() {
-
   /* esto lo agregó Ricardo */
   /**SE DEBEN AGREGAR LOS DEMÁS DATOS SI APLICA */
   var username = $("#input_username_registro").val();
   console.log("🚀 ~ file: script_registro.js:21 ~ login ~ username:", username);
-  if (username === "") {Swal.fire("Debe ingresar nombre de usuario!", "", "error");return;}
+  if (username === "") {
+    Swal.fire("Debe ingresar nombre de usuario!", "", "error");
+    return;
+  }
 
   var email = $("#input_email_registro").val();
-  console.log("🚀 ~ file: script_registro.js:28 ~ login ~ email:",email);
-  if (email === "") {Swal.fire("Debe ingresar email!","El campo de email es obligatorio!!","warning");return;}
-
+  console.log("🚀 ~ file: script_registro.js:28 ~ login ~ email:", email);
+  if (email === "") {
+    Swal.fire(
+      "Debe ingresar email!",
+      "El campo de email es obligatorio!!",
+      "warning"
+    );
+    return;
+  }
 
   var password = $("#input_password_registro").val();
-  console.log("🚀 ~ file: script_registro.js:28 ~ login ~ password:",password);
-  if (password === "") {Swal.fire("Debe ingresar password!", "El campo de password es obligatorio!!","warning"); return;}
+  console.log("🚀 ~ file: script_registro.js:28 ~ login ~ password:", password);
+  if (password === "") {
+    Swal.fire(
+      "Debe ingresar password!",
+      "El campo de password es obligatorio!!",
+      "warning"
+    );
+    return;
+  }
 
   datos = { username, email, password };
   console.log("🚀 ~ file: script_registro.js:44 ~ login ~ datos:", datos);
@@ -104,7 +159,7 @@ function register() {
 
   $.ajax({
     data: datos,
-    url: "http://localhost/SmartParkingEnterprise/registrarusuario.php",//antes era checklogin
+    url: "http://localhost/SmartParkingEnterprise/registrarusuario.php", //antes era checklogin
     //cache: false,
     dataType: "json",
     type: "POST",
@@ -113,10 +168,26 @@ function register() {
     },
     success: function (respuesta) {
       console.log("rta: ", respuesta);
-      Swal.fire("" + respuesta.message, "", "success");
       /***acá se manejan los estados del servidor o las rtas del server */
+
+      /* CASOS EN LOS QUE LA RTA DEL SERVDIOR ES EXITOSA */
+      if (respuesta.error === "El usuario ya se encuentra registrado!!") {
+        Swal.fire("" + respuesta.error, "", "warning");
+      }
+      if (respuesta.message === "registroExitoso") {
+        Swal.fire("!El registro ha sido exitoso!", "", "success");
+      }
+      if (respuesta.error === "Error al crear el usuario") {
+        Swal.fire("" + respuesta.error, "", "error");
+      }
+      /* SI HAY ERROR DE CONEXIÓN CON LA BD */
+      if (respuesta.dbError !== "") {
+        Swal.fire("" + respuesta.dbError, "", "error");
+        return;
+      }
     },
     error: function (jqXHR, textStatus) {
+      console.log(textStatus);
       Swal.fire(
         "!Parece que hay un error en la solicitud AJAX¡",
         "" + textStatus,
@@ -125,4 +196,3 @@ function register() {
     },
   });
 }
-
